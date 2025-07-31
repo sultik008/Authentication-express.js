31a881be00371532d3f86815bdb2e99d4c5f8fd5
Sultanbek <sultanbekzumamuratov171@gmail.com>
2025-07-31 15:50:26 +0500

    fixing route

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const __dirname = path.dirname(__filename);
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "build")));
 app.use(router)
+app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
