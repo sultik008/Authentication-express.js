1df5c059c5c109b2ca99ca8bc40b308b316be313
Sultanbek <sultanbekzumamuratov171@gmail.com>
2025-07-31 15:18:18 +0500

    Backend sends frontend

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ dotenv.config();
 import express from 'express'
 import router from './routers/router.js'
 import cors from 'cors'
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, "authentication/build")));
 app.use(router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "authentication/build", "index.html"));
+});
 app.listen(5000, () => console.log("Express connected to http://localhost:5000"));
 
 
